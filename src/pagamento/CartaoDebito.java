7ddcf60a6005a9d209f6dd59553e321a6aa92b5a
sarasouzav <128659890+sarasouzav@users.noreply.github.com>
2023-10-05 22:42:07 -0300

    Update CartaoDebito.java

--- src/pagamento/CartaoDebito.java
+++ src/pagamento/CartaoDebito.java
@@ -1,21 +1,20 @@
 package pagamento;
 
-public abstract  class CartaoDebito  extends Pagamento {
-
+public class CartaoDebito extends Pagamento{
     private String numCartao;
     private String titularCartao;
 
-    public CartaoDebito(String dataHoraPagamento, int numeroPagamento, double valorPago) {
+    public CartaoDebito(String dataHoraPagamento, int numeroPagamento, double valorPago, String numCartao, String titularCartao) {
         super(dataHoraPagamento, numeroPagamento, valorPago);
+        this.numCartao = numCartao;
+        this.titularCartao = titularCartao;
     }
 
-
     public String getNumCartao() {
-
         return numCartao;
     }
 
-    public void setNumCartao (String numCartao) {
+    public void setNumCartao(String numCartao) {
         this.numCartao = numCartao;
     }
 
@@ -25,6 +24,15 @@ public abstract  class CartaoDebito  extends Pagamento {
 
     public void setTitularCartao(String titularCartao) {
         this.titularCartao = titularCartao;
+    }
+
+    @Override
+    public String imprimirCupomFiscal() {
+        return "Data e Hora: " + getDataHoraPagamento() +
+               "\n Número Pagamento: " + getNumeroPagamento() +
+               "\n Valor Pago: " + getValorPago() +
+               "\n Número Cartão: " + getNumCartao() +
+               "\n Titular Cartão: " + getTitularCartao();
     }
 
 }
