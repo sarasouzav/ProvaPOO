5942a250ab102335759508768c274f00081a02c7
sarasouzav <128659890+sarasouzav@users.noreply.github.com>
2023-10-05 22:48:18 -0300

    Update Professor.java

--- src/Pessoa/Professor.java
+++ src/Pessoa/Professor.java
@@ -1,4 +1,38 @@
 package Pessoa;
 
-public class Professor {
+public class Professor extends Pessoa{
+    private float salarioHora;
+    private int horasAula;
+
+    public Professor(String nome, String email, String senha, float salarioHora, int horasAula) {
+        super(nome, email, senha);
+        this.salarioHora = salarioHora;
+        this.horasAula = horasAula;
+    }
+
+    public float getSalarioHora() {
+        return salarioHora;
+    }
+
+    public void setSalarioHora(float salarioHora) {
+        this.salarioHora = salarioHora;
+    }
+
+    public int getHorasAula() {
+        return horasAula;
+    }
+
+    public void setHorasAula(int horasAula) {
+        this.horasAula = horasAula;
+    }
+
+    public float calculaSalario(){
+        return getSalarioHora() * getHorasAula();
+    }
+
+    public String mostraProfessor(){
+        return "Nome: " + getNome() +
+                "E-mail: " + getEmail() +
+                "Salário: " + calculaSalario();
+    }
 }
