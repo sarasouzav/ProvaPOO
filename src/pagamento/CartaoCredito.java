61084f5cae5b7c751aa1a0a4356f16733af9800c
sarasouzav <128659890+sarasouzav@users.noreply.github.com>
2023-10-05 22:43:32 -0300

    Update CartaoCredito.java

--- src/pagamento/CartaoCredito.java
+++ src/pagamento/CartaoCredito.java
@@ -1,25 +1,22 @@
-package pagamento;
+package questao1;
 
-public abstract class CartaoCredito {
-
+public class CartaoCredito extends Pagamento{
     private String numCartao;
     private String bandeiraCartao;
     private String titularCartao;
 
-    public CartaoCredito(  String numCartao,  String bandeiraCartao , String titularCartao) {
-
+    public CartaoCredito(String dataHoraPagamento, int numeroPagamento, double valorPago, String numCartao, String bandeiraCartao, String titularCartao) {
+        super(dataHoraPagamento, numeroPagamento, valorPago);
         this.numCartao = numCartao;
         this.bandeiraCartao = bandeiraCartao;
         this.titularCartao = titularCartao;
-
     }
 
     public String getNumCartao() {
-
         return numCartao;
     }
 
-    public void setNumCartao (String numCartao) {
+    public void setNumCartao(String numCartao) {
         this.numCartao = numCartao;
     }
 
@@ -39,5 +36,14 @@ public abstract class CartaoCredito {
         this.titularCartao = titularCartao;
     }
 
+    @Override
+    public String imprimirCupomFiscal() {
+        return "Data e Hora: " + getDataHoraPagamento() +
+               "\n Número Pagamento: " + getNumeroPagamento() +
+               "\n Valor Pago: " + getValorPago() +
+               "\n Número Cartão: " + getNumCartao() +
+               "\n Bandeira Cartão: " + getBandeiraCartao() +
+               "\n Titular Cartão: " + getTitularCartao();
 
+    }
 }
